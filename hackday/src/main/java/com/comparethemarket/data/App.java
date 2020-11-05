97bd0c0f80af702d6f5eba49d2aa80a5f01d0c99
Amrita Bhattacharjee <amrita.bhattacharjee@bglgroup.co.uk>
2020-11-05 10:24:11 +0000

    first commit

---
+++ hackday/src/main/java/com/comparethemarket/data/App.java
@@ -0,0 +1,13 @@
+package com.comparethemarket.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
