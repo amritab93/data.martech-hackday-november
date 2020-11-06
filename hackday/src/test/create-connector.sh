0a05871a9df3cf92836dafb7f79d245d2a5eef1a
Amrita Bhattacharjee <amrita.bhattacharjee@bglgroup.co.uk>
2020-11-06 17:17:31 +0000

    Fix curl command

--- hackday/src/test/create-connector.sh
+++ hackday/src/test/create-connector.sh
@@ -3,8 +3,6 @@ set -e
 
 STATE=NOT_RUNNING
 
-CONNECTOR_CONFIG=../connectors/postgres.json
-
-curl -X POST -H "Content-Type: application/json" --data @test/connectors/postgres.json ${KAFKA_CONNECT_URL}/connectors
+curl -X POST -H "Content-Type: application/json" --data @connectors/postgres.json ${KAFKA_CONNECT_URL}/connectors
 
 STATE=$(docker-compose -f ./docker-compose.yml exec -T kafka-connect curl -X GET -H "Content-Type: application/json" "$KAFKA_CONNECT_URL"/connectors/"$NAME"/status | jq -r '.tasks[0].state')
